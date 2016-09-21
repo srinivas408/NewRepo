de60f8d4eafa0974179a4a7680733f18c994de00
srinivasa reddy <srinivasa reddy>
2016-09-21 16:04:53 +0530

    some more lines added to Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ System.out.println("hello from git s/w");
 System.out.println("bye from git s/w");
 System.out.println("good mornig from github website");
 System.out.println("good evening  from github website");
+System.out.println("night from git s/w");
+System.out.println("late night from git s/w");
 }
 }
