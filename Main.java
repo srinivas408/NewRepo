1ca68fb38e8b0307afc74b71ec7c0e60be684054
srinivasa reddy <srinivasa reddy>
2016-09-21 15:57:22 +0530

    a new line of code is added in Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class  Main{
 public static void main(String[] args){
 System.out.println("hai from git s/w");
+System.out.println("hello from git s/w");
+System.out.println("bye from git s/w");
 }
 }
